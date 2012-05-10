9e4475644538430a962b0e625734327c7405abfa
Jeonghwan Jang <inomies@gmail.com>
2012-05-10 21:48:04 +0900

    add collision logic - uncompleted

--- pingpong/js/pingpong.js
+++ pingpong/js/pingpong.js
@@ -1,6 +1,18 @@
-var pingpong = {};
+var pingpong = {
+	scoreA: 0,
+	scoreB: 0
+};
 
 pingpong.pressedKeys = [];
+
+pingpong.ball = {
+	diameter: 10,
+	speed: 2,
+	x: 150,
+	y: 100,
+	directionX: 1,
+	directionY: 1
+};
 
 
 var KEY = {
@@ -12,7 +24,76 @@ var KEY = {
 
 
 function gameloop(){
+	moveBall();
 	movePaddles();
+}
+
+function moveBall(){
+	var playgroundHeight = parseInt($Element("playground").height());
+	var playgroundWidth = parseInt($Element("playground").width());
+	var ball = pingpong.ball;
+
+	if(ball.y + ball.speed * ball.directionY + ball.diameter > playgroundHeight){
+		ball.directionY = -1;
+	}
+
+	if(ball.y + ball.speed * ball.directionY < 0){
+		ball.directionY = 1;
+	}
+
+	if(ball.x + ball.speed * ball.directionX + ball.diameter > playgroundWidth){
+		pingpong.scoreA++;
+		$Element("scoreA").html(pingpong.scoreA);
+
+		ball.x = 250;
+		ball.y = 100;
+		$Element("ball").css({
+			"left": ball.x,
+			"top": ball.y
+		});
+		ball.directionX = -1;
+	}
+
+	if(ball.x + ball.speed * ball.directionX < 0){
+		pingpong.scoreB++;
+		$Element("scoreB").html(pingpong.scoreB);
+
+		ball.x = 250;
+		ball.y = 100;
+		$Element("ball").css({
+			"left": ball.x,
+			"top": ball.y
+		});
+		ball.directionX = 1;
+	}
+
+	ball.x += ball.speed * ball.directionX;
+	ball.y += ball.speed * ball.directionY;
+
+	var paddleAX = parseInt($Element("paddleA").css("left")) + parseInt($Element("paddleA").css("width"));
+	var paddleAYBottom = parseInt($Element("paddleA").css("top")) + parseInt($Element("paddleA").css("height"));
+	var paddleAYTop = parseInt($Element("paddleA").css("top"));
+
+	if(ball.x + ball.speed * ball.directionX < paddleAX){
+		if(ball.y + ball.speed * ball.directionY <= paddleAYBottom &&
+			ball.y + ball.speed * ball.directionY >= paddleAYTop){
+			ball.directionX = 1;
+		}
+	}
+
+	var paddleBX = parseInt($Element("paddleB").css("left"));
+	var paddleBYBottom = parseInt($Element("paddleB").css("top")) + parseInt($Element("paddleB").css("height"));
+	var paddleBYTop = parseInt($Element("paddleB").css("top"));
+
+	if(ball.x + ball.speed * ball.directionX >= paddleBX){
+		if(ball.y + ball.speed * ball.directionY <= paddleBYBottom &&
+			ball.y + ball.speed * ball.directionY >= paddleBYTop){
+			ball.directionX = -1;
+		}
+	}
+
+	$Element("ball").css({"left": ball.x, "top": ball.y});
+
 }
 
 function movePaddles(){
